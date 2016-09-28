777d570c250910e155e96d31944502e66136282c
Diego <diego@Mate>
2016-09-28 11:56:45 -0300

    estadisticas

---
+++ apps/alumnos/modules/informes/templates/estadisticasSuccess.php
@@ -0,0 +1,15 @@
+<br>
+<h1 align="center" style="color:black;">Padron de Socios Activos</h1>
+
+ <a target="_blank" href="<?php echo url_for('informes/padronsocios') ?>"><img src='<?php echo $sf_request->getRelativeUrlRoot();?>/images/printer.png' align='center' size='20' /></a>
+<table cellspacing="0" class="stats">
+     <tr>
+                  <td width="30%" align="center" class="hed">Nombre</td>
+                  <td width="20%" align="center" class="hed">Dirección</td>
+                  <td width="10%" align="center" class="hed">Teléfono</td>
+                  <td width="10%" align="center" class="hed">Móvil</td>
+                  <td width="25%" align="center" class="hed">Email</td>
+      </tr>
+    <tbody>
+      
+  </table>
