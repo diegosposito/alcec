71966f024dfe613bef128ff552cf17d952ca34aa
diego <spositod@gmail.com>
2017-01-31 11:19:24 -0300

    cabmios

--- apps/alumnos/modules/informes/templates/sociosSuccess.php
+++ apps/alumnos/modules/informes/templates/sociosSuccess.php
@@ -32,19 +32,19 @@
                   <td width="30%" align="center" class="hed">Nombre</td>
                   <td width="20%" align="center" class="hed">Dirección</td>
                   <td width="10%" align="center" class="hed">Teléfono</td>
-                  <td width="10%" align="center" class="hed">Móvil</td>
-                  <td width="25%" align="center" class="hed">Email</td>
+                  <td width="10%" align="center" class="hed">Monto</td>
+                  <td width="25%" align="center" class="hed">Pago</td>
       </tr>
     <tbody>
       <?php $i=0; $ciudad='NULL';?>
       <?php foreach ($profesionaless as $profesionales){ ?>
   
       <tr class="fila_<?php echo $i%2 ; ?>">
-          <td width="30%" align="left"><?php echo $profesionales->getApellido().', '.$profesionales->getNombre()  ?></td>
-          <td width="20%" align="left"><?php echo $profesionales->getDireccion()  ?></td>
-          <td width="10%" align="left"><?php echo $profesionales->getTelefono()  ?></td>
-          <td width="10%" align="left"><?php echo $profesionales->getCelular() ?></td>
-          <td width="25%" align="left"><?php echo $profesionales->getEmail() ?></td>
+          <td width="30%" align="left"><?php echo $profesionales['apellido'].', '.$profesionales['nombre']  ?></td>
+          <td width="20%" align="left"><?php echo $profesionales['direccion']  ?></td>
+          <td width="10%" align="left"><?php echo $profesionales['telefono']  ?></td>
+          <td width="10%" align="left"><?php echo $profesionales['monto'] ?></td>
+          <td width="25%" align="left"><?php echo $profesionales['tipopago'] ?></td>
       </tr>
       <?php $i++; ?>
       <?php } ?>
