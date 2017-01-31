685c387ed7cb904e2164499d18c41c368b7d7421
diego <spositod@gmail.com>
2017-01-31 11:19:55 -0300

    cabmios

--- apps/alumnos/modules/personas/templates/generarrecibosSuccess.php
+++ apps/alumnos/modules/personas/templates/generarrecibosSuccess.php
@@ -4,7 +4,7 @@ $(document).ready(function(){
     $('#botonlista').click(function() {
     // guardar la informacion de documentacion adicional del alumnos ingresada
         $.post("<?php echo url_for('personas/obtenerrecibosgenerados'); ?>",
-          { permite_seleccionar: '1'},
+          { permite_seleccionar: '1', idmes: $('#idmes').val(), idanio: $('#idanio').val()},
         function(data){
             $('#idresultados').html(data);
         }
@@ -27,6 +27,24 @@ $(document).ready(function(){
 <form action="<?php echo url_for('personas/grabarrecibosgenerados') ?>" method="post" id="formLista">
   <table cellspacing="0" class="stats" width="100%">
     <tfoot>
+      <tr>
+      <td align="center" colspan="4"><b>Mes     : </b>
+      <select id="idmes" name="idmes">
+      <?php for ($mes = 1; $mes <= 12; $mes++) { ?>
+        <option value="<?php echo $mes ?>" <?php if ($mes == $messeleccionado) { ?>selected<?php } ?>><?php echo $mes ?></option>
+      <?php } ?>
+      </select>        
+      </td>
+      </tr> 
+      <tr>
+      <td align="center" colspan="4"><b>Año     : </b>
+      <select id="idanio" name="idanio">
+      <?php for ($anio = date("Y")-1; $anio < date("Y")+2; $anio=$anio+1) { ?>
+        <option value="<?php echo $anio ?>" <?php if ($anio == $anioseleccionado) { ?>selected<?php } ?>><?php echo $anio ?></option>
+      <?php } ?>
+      </select>        
+      </td>
+      </tr>     
       <tr>
         <td colspan="2" align="center">
           <input type="button" value="Mostrar recibos a generar" name="botonlista" id="botonlista" />
