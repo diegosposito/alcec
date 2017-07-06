4a5e54f00a940d6e9895ff031b08df2886480eac
Sposito, Diego <spositod@gmail.com>
2017-07-06 11:50:20 -0300

    cam

--- lib/form/doctrine/BuscarAlumnosForm.class.php
+++ lib/form/doctrine/BuscarAlumnosForm.class.php
@@ -1,10 +1,10 @@
 <?php
 class BuscarAlumnosForm extends sfForm
 {
-  protected static $tiposcriterios = array(1 => "Apellido", 2 => "Nro. Documento");
-	        
+  protected static $tiposcriterios = array(1 => "Apellido", 2 => "Nro. Documento", 3 => "Dirección");
+
   public function configure()
-  {        
+  {
   	$idarea = sfContext::getInstance()->getUser()->getAttribute('id_area','');
   	$carreras = Doctrine_Core::getTable('AreasCarrera')->obtenerCarrerasPorArea(1);
   	//$arr = array(1, 2, 6);
@@ -19,32 +19,32 @@ class BuscarAlumnosForm extends sfForm
   		}
   	}
 	asort($arregloPlanes);
-	
+
 	// Se define el esquema del form
   	//$this->widgetSchema['idplanestudio'] = new sfWidgetFormSelect(array('choices' => $arregloPlanes));
 	$this->widgetSchema['tipocriterio'] = new sfWidgetFormSelect(array('choices' => self::$tiposcriterios));
 	$this->widgetSchema['criterio'] = new sfWidgetFormInput();
 	$this->widgetSchema['referer'] = new sfWidgetFormInputHidden();
-	$this->widgetSchema['url'] = new sfWidgetFormInputHidden();	
+	$this->widgetSchema['url'] = new sfWidgetFormInputHidden();
 	$this->widgetSchema['titulo'] = new sfWidgetFormInputHidden();
 	$this->widgetSchema['tipo'] = new sfWidgetFormInputHidden();
-	
+
  	// Se define los labels
 	//$this->widgetSchema->setLabel('idplanestudio', '<p align="left">Carrera:</p>');
  	$this->widgetSchema->setLabel('tipocriterio', '<p align="left">Buscar en:</p>');
  	$this->widgetSchema->setLabel('criterio', '<p align="left"></p>');
 
- 	// Se define los validadores 
+ 	// Se define los validadores
 	$this->setValidators(array(
 	//	'idplanestudio'    => new sfValidatorString(),
-		'tipocriterio'    => new sfValidatorString(),	
+		'tipocriterio'    => new sfValidatorString(),
 		'criterio'    => new sfValidatorString(array('required' => false)),
 		'url' => new sfValidatorString(array('required' => false)),
 		'referer' => new sfValidatorString(array('required' => false)),
 	));
- 
+
 	$this->widgetSchema->setNameFormat('buscar[%s]');
-	
-	$this->validatorSchema->setOption('allow_extra_fields',true); 	
+
+	$this->validatorSchema->setOption('allow_extra_fields',true);
   }
 }
